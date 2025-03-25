d7d7f095c092e0d4ea066ce1259a928dae33618e
Jeevan Raj <jeevan.raj@abc.courses>
2025-03-25 11:56:39 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const path = require('path');
-const cors =require('cors');
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Serve static files from the React frontend build
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-
 app.use(cors());
-// API route to send JSON response
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
-// Catch-all route to serve the React app for any other request
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-// Start the server and listen on the specified port
 app.listen(port, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${port}`);
 });
