fd140ff826a0823d2a230031e3234a29656f6fe3
Jeevan Raj <jeevan.raj@abc.courses>
2025-03-25 12:13:22 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 5000;
